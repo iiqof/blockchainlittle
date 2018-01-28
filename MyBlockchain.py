525f03db90e62ed368be38fc7810ca8aae478b2e
iiqof <iiqof@users.noreply.github.com>
2018-01-28 17:38:34 +0000

    Add files via upload

--- MyBlockchain.py
+++ MyBlockchain.py
@@ -1,25 +1,46 @@
 from hashlib import sha256
+from itertools import chain, product
 
 
-# The data has to be the most basic ascii, as the read() function does not take special characters like Ñ or à. Also @ $ % are reserved characters that marck structure on a block.
+# The data has to be the most basic ascii, as the read() function does not take special characters like enye or acento. Also @ $ % are reserved characters that marck structure on a block.
 
 #TODO:
-# Add saveguards for @ $ %    
-# Add blockchain analysis (size,...)    
+# 2 Add saveguards for @ $ %    
+# 3 Add blockchain analysis (size,...)    
  
+ 
+characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+NNN = 100  # Maximum lenght of the tried nonces
+
+
+def bruteforce(charset, maxlength):
+    return (''.join(candidate)
+        for candidate in chain.from_iterable(product(charset, repeat=i)
+        for i in range(1, maxlength + 1)))
+ 
+
 class Block:
     
-    def __init__(self, bindex, bdata, prebhash):
+    def __init__(self, bindex, bdata, prebhash, nonce=0):
         self.bindex = bindex
         self.bdata = bdata
         self.prebhash = prebhash
-        self.bhash = self.get_hash()
+        self.nonce = nonce
     
     def get_hash(self):
-        data = str(self.bindex) + str(self.bdata) + str(self.prebhash)
+        data = '?' + str(self.nonce) + '@' + str(self.bindex) + '%' + \
+            str(self.bdata) + '$' + str(self.prebhash)
         sha=sha256()
         sha.update(data)
         return sha.hexdigest()
+    
+    def get_nonce(self, difty=0):
+        pofw = str(0)*difty
+        for attempt in bruteforce(characters, NNN):
+            self.nonce = attempt
+            if str(self.get_hash())[:difty] == pofw:
+                break
+        return 0
         
     def display(self):
         displayb(self)
@@ -27,10 +48,11 @@ class Block:
     
 
 def createb(bdata, preb):
-    return Block(preb.bindex + 1, bdata, preb.bhash)
+    return Block(preb.bindex + 1, bdata, preb.get_hash())
     
 
 def displayb(b):
+    print("Nonce:              " + str(b.nonce))
     print("Index:              " + str(b.bindex))
     print("Data:               " + str(b.bdata))
     print("Previous Block Hash:" + str(b.prebhash) + "\n")
@@ -58,14 +80,19 @@ class BlockChain:
             print "Error: Not initilaized"
             return -1
 
-    def nextb(self,data):
-        newb = createb(data,self.headb())
-        self.addb(newb)
+    def accb(self, b, difty):
+        if str(b.get_hash())[:difty] == str(0)*difty:
+            self.addb(b)
+        else:
+            print('Invalid nonce')
+            return -1
         return 0
-    
-    def initc(self):
+        
+    def initc(self,diffty=0):
         self.clenght = 1
-        self.chain.append(createGenb())
+        Genb = createGenb()
+        Genb.get_nonce(diffty)
+        self.chain.append(Genb)
     
     def display(self,opt=0):
         if opt == 0:
@@ -79,7 +106,11 @@ class BlockChain:
         else:
             clittle = ""
             for i in self.chain:
-                clittle = clittle + "@" + str(i.bindex)+"%"+str(i.bdata)+"$"+str(i.prebhash)
+                clittle = clittle + ( 
+                 "#" + str(i.nonce) 
+                + "@" + str(i.bindex) 
+                + "%" + str(i.bdata) 
+                + "$" + str(i.prebhash) )
             return clittle
 
     def save(self, name="blockchain"):
@@ -92,13 +123,12 @@ def loadc(name):
     f = open(str(name), 'r')
     cstring = f.read()
     BC = BlockChain()
-    for block in cstring.split("@")[1:]:
-        split_block1 = block.split('%')
-        split_block2 = split_block1[1].split('$')
-        BC.chain.append(Block(int(split_block1[0]), str(split_block2[0]), split_block2[1] ))
+    for block in cstring.split("#")[1:]:
+        split_block1 = block.split('@')
+        split_block2 = split_block1[1].split('%')
+        split_block3 = split_block2[1].split('$')
+        BC.chain.append(Block(int(split_block2[0]), str(split_block3[0]), split_block3[1], split_block1[0] ))
         BC.clenght = len(BC.chain)
     return BC
     
-
-
 
